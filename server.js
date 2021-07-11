1c30a6d082b9b2be96c7a101393637865573d00f
lovelygracearsolon <lovelygracearsolon@gmail.com>
2021-07-12 01:33:14 +0800

    [ADD MongoDB connection

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 app.get("/", (req, res) => {
   res.send("MERN Chat App");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const db = process.env.MONGODB_URI;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
